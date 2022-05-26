40df4efd829bad5d68ac674caa5798ea3ff2b593
AliAbdulloh <74522999+AliAbdullo@users.noreply.github.com>
2022-05-26 05:38:01 +0000

    Ustoz korsatib o'tgan misollarni amalyotda o'rganib ko'nikma oldim

---
+++ Xatolar02.py
@@ -0,0 +1,18 @@
+"""
+26/05/2022
+
+Dasturlash asoslari
+
+#35-DARS. XATOLAR BILAN ISHLASH
+
+Muallif: Anvar Narzullaev
+
+Web sahifa: https://python.sariq.dev
+"""
+while True:
+    yosh = input("Yoshingizni kiriting: ")
+    if yosh.isdigit():
+        yosh = int(yosh)
+        break
+
+print(f"Siz {2021-yosh} yilda tug'ilgansiz")
